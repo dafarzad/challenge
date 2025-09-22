3b0ee0cd9670b5005568eb4e4c4b904e2b8b212f
farzad dadkhah <dafarzad@gmail.com>
2025-09-22 13:38:05 +0330

    fix a bug

--- src/Lottery.Application/DTOs/RegisterRequestDto.cs
+++ src/Lottery.Application/DTOs/RegisterRequestDto.cs
@@ -4,13 +4,13 @@ namespace Lottery.Application.DTOs;
 
 public class RegisterRequestDto
 {
-    [Required]
+    [Required, StringLength(100, MinimumLength = 1)]
     public string FirstName { get; set; } = string.Empty;
-    [Required]
+    [Required, StringLength(100, MinimumLength = 1)]
     public string LastName { get; set; } = string.Empty;
-    [Required]
+    [Required, StringLength(15, MinimumLength = 7)]
     public string Phone { get; set; } = string.Empty;
-    [Required]
+    [Required, StringLength(10, MinimumLength = 10)]
     public string NationalCode { get; set; } = string.Empty;
     [Required]
     public int CampaignId { get; set; }
